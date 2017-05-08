a44aa1c93823774b8bd3e548f0272946d13c3783
Jake Adams <jadams.html@gmail.com>
2017-05-08 20:13:59 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+
+$(() => {
+
+  $(".button").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#about").offset().top
+      }, 1000);
+      $('#p1').animate({
+          scrollTop: $("main").offset().top},
+          'slow');
+  });
+
+  $(".button2").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#work").offset().top
+      }, 1000);
+      $('#p2').animate({
+          scrollTop: $("main").offset().top},
+          'slow');
+  });
+
+  $(".button3").click(function() {
+      $('html, body').animate({
+          scrollTop: $("#contact").offset().top
+      }, 1000);
+  });
+
+  $(document).ready(function(){
+    $(this).scrollTop(0);
+  });
+
+
+});
